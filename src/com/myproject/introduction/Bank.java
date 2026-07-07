f9143bfebead5aa23fe47a9c8fee0923d39089a1
Chinmay <chinmaygope69@gmail.com>
2026-07-07 12:43:32 +0530

    credit and debit

--- src/com/myproject/introduction/Bank.java
+++ src/com/myproject/introduction/Bank.java
@@ -5,16 +5,34 @@ public class Bank {
     String customerName;
     double accountBalance;
 
-    {
-        accountNumber++;
+    void getBalance() {
+        System.out.println("Account balance is " + accountBalance);
+    }
+
+    void deposit(double amount) {
+        System.out.println("Deposited " + amount + " into account " + accountNumber);
+        accountBalance = accountBalance + amount;
+        getBalance();
+    }
+
+    void withdraw(double amount) {
+        System.out.println("Withdraw amount is " + amount);
+        if (amount <= accountBalance) {
+            accountBalance = accountBalance - amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+        getBalance();
     }
 
     public Bank(String customerName, double accountBalance) {
         this.customerName = customerName;
         this.accountBalance = accountBalance;
+        accountNumber++;
     }
 
     void showDetails() {
+        System.out.println("___________________________");
         System.out.println("Account Number: " + Bank.accountNumber);
         System.out.println("Customer Name: " + customerName);
         System.out.println("Account Balance: " + accountBalance);
@@ -24,12 +42,18 @@ public class Bank {
 
     static void main() {
         Bank bank = new Bank("Jack", 100);
-        bank.showDetails();
 
-        Bank bank2 = new Bank("John", 350);
+        bank.getBalance();
+        bank.deposit(100);
+        bank.deposit(200);
+        bank.showDetails();
+        bank.withdraw(400);
+        bank.withdraw(200);
+
+        Bank bank2 = new Bank("Bob", 100);
         bank2.showDetails();
 
-        Bank bank3 = new Bank("Tony", 400);
+        Bank bank3 = new Bank("John", 100);
         bank3.showDetails();
     }
 }
