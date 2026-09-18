43c4f4b42617064d74b890ff5d133bb518f2da5e
Chinmay <235725763+chinmay-gope@users.noreply.github.com>
2026-09-18 05:44:17 +0530

    Optimize climbStairs method: reduce space complexity by eliminating the dynamic programming array and using two variables for calculation

---
+++ src/com/myproject/data_structures/dp/HouseRobber.java
@@ -0,0 +1,80 @@
+package com.myproject.data_structures.dp;
+
+public class HouseRobber {
+
+    static void main() {
+        int[] nums = {2, 7, 9, 3, 1};
+        System.out.println("rob(nums) = " + rob(nums));
+        System.out.println("robOpt(nums) = " + robOpt(nums)); // 12
+
+        System.out.println("cyclicRob(nums) = " + cyclicRob(nums)); // 11
+    }
+
+    public static int rob(int[] nums) {
+/*      int n = nums.length;
+        if (n == 1) return nums[0];
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]); // max(inc, exc)
+        }
+
+        return dp[n - 1];
+*/
+        return rob(nums, 0, nums.length - 1);
+    }
+
+    public static int robOpt(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+
+        int prev1 = nums[0];
+        int prev2 = Math.max(nums[0], nums[1]);
+        int res = prev2;
+
+        for (int i = 2; i < n; i++) {
+            res = Math.max(prev1 + nums[i], prev2);
+            prev1 = prev2;
+            prev2 = res;
+        }
+
+        return res;
+    }
+
+    //    HOUSE ROBBER - 2 (houses are cyclic)
+    public static int cyclicRob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+
+        return Math.max(
+                rob(nums, 0, nums.length - 2),
+                rob(nums, 1, nums.length - 1)
+        );
+    }
+
+    private static int rob(int[] nums, int st, int end) {
+/*      int[] dp = new int[nums.length - 1];
+        dp[0] = nums[st];
+        dp[1] = Math.max(nums[st], nums[st + 1]);
+*/
+        int prev1 = nums[st];
+        int prev2 = Math.max(nums[st], nums[st + 1]);
+        int res = prev2;
+
+        for (int i = st + 2, j = 2; i <= end; i++, j++) {
+//            dp[j] = Math.max(nums[i] + dp[j - 2], dp[j - 1]); // {inc, exc}
+            res = Math.max(prev1 + nums[i], prev2);
+
+            prev1 = prev2;
+            prev2 = res;
+        }
+
+//        return dp[n - 2];
+        return res;
+    }
+
+}
